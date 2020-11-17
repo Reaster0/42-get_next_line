8d4f45f1833b5b3b53e5d079b6269925f4093a80
Reaster <darcrai11@gmail.com>
2020-11-17 20:03:16 +0100

    initial commit

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: earnaud <earnaud@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/17 12:49:51 by earnaud           #+#    #+#             */
+/*   Updated: 2020/11/17 14:24:42 by earnaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+void	ft_putstr_fd(char *s, int fd);
+
+#endif
